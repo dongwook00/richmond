daf24b8a95a0378ef1720fad5160c09615d3c244
Dongwook Kim <dongwook227@gmail.com>
2022-09-11 16:38:18 -0400

    add Header component

---
+++ components/Header.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <header className="flex justify-between items-center mb-10">
+      <Link href="/">
+        <a>
+          <h1 className="text-3xl font-black">Helloteemo</h1>
+        </a>
+      </Link>
+      <div className="text-2xl">☀️🌛</div>
+    </header>
+  );
+}
--- components/layout.js
+++ components/layout.js
@@ -1,14 +1,10 @@
-import Link from "next/link";
+import Header from "./Header";
 
 export default function Layout({ children }) {
   return (
     <div className="min-h-screen">
       <div className="mx-auto max-w-2xl py-11 px-6">
-        <header className="mb-8 py-4">
-          <Link href="/">
-            <h3>Helloteemo</h3>
-          </Link>
-        </header>
+        <Header />
         <main className="container mx-auto flex-1">{children}</main>
         {/* <footer className="mt-8 py-4">
         <div className="container mx-auto flex justify-center">
