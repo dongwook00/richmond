ddaf3b6f1553f2d9f1b11ad0698ab6a1d086651a
Dongwook Kim <dongwook227@gmail.com>
2022-09-12 20:03:05 -0400

    update aside spaces

--- components/Aside.js
+++ components/Aside.js
@@ -3,18 +3,19 @@ import Image from "next/image";
 export default function Aside() {
   return (
     <aside>
-      <div className="flex mb-14">
+      <div className="flex mb-14 gap-x-3.5">
         <Image
           className="rounded-full"
+          layout="fixed"
           width={56}
           height={56}
           alt="profile"
           src="/images/profile.jpg"
         />
-        <p className="ml-3.5 max-w-xs leading-relaxed">
+        <p className="max-w-xs leading-relaxed mb-7">
           프론트엔드 개발자
           <br />
-          미국에서의 회사 생활을 기록합니다.
+          미국에서의 회사생활을 공유합니다
         </p>
       </div>
     </aside>
