ff012bef7076ee6e21efb9113836b42b8b33a665
Dongwook Kim <dongwook227@gmail.com>
2022-09-12 20:37:26 -0400

    updated layout

--- components/layout.js
+++ components/layout.js
@@ -1,13 +1,27 @@
+import { useRouter } from "next/router";
 import Header from "./Header";
 import Aside from "./Aside";
 
 export default function Layout({ children }) {
+  const router = useRouter();
+  const isHome = router.pathname === "/";
+
   return (
     <div className="min-h-screen">
       <div className="mx-auto max-w-2xl py-11 px-6">
-        <Header />
-        <Aside />
-        <main>{children}</main>
+        {isHome ? (
+          <>
+            <Header />
+            <Aside />
+            <main>{children}</main>
+          </>
+        ) : (
+          <>
+            <Header />
+            <main>{children}</main>
+            <Aside />
+          </>
+        )}
       </div>
     </div>
   );
